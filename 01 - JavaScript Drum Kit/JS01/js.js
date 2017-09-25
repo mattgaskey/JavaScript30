1a6a246cf12f9f4f892e5c786f7ef26ee78dc373
mattgaskey <mattgaskey@gmail.com>
2017-09-25 15:32:02 +0300

    Soltuion added

---
+++ 01 - JavaScript Drum Kit/JS01/js.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+	const key = event.which || event.keyCode;
+	const beat = document.querySelectorAll(`[data-key="${key}"`);
+	if (beat.length > 0) {
+		event.preventDefault();
+		const audio = new Audio(beat[1].src);
+		audio.play();
+		beat[0].classList.add('playing');
+	}
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
